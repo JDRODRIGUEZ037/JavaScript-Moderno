5fdc09f321cdfea9bc7c5b44baf540a6b696543b
JDRODRIGUEZ037 <juandavidrodriguez037@gmail.com>
2023-11-29 11:58:35 -0500

    section 2

---
+++ 0.1-Fundamentos/assets/js/alerts.js
@@ -0,0 +1,14 @@
+
+
+
+alert('Hola Mundo');
+
+
+let nombre = prompt('¿Cual es tu nombre?');
+console.log('****' + nombre + '****');
+console.log(nombre);
+
+const seleccion = confirm('¿Estas seguro de realizar esta accion?');
+console.log(confirm)
+
+// console.log( global );
