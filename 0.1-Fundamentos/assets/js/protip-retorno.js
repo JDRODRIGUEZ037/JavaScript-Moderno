2045152b1bef8d77a26b16df6f9a41795a922b90
JDRODRIGUEZ037 <juandavidrodriguez037@gmail.com>
2023-12-15 11:53:10 -0500

    f pt

--- 0.1-Fundamentos/assets/js/protip-retorno.js
+++ 0.1-Fundamentos/assets/js/protip-retorno.js
@@ -1,19 +1,22 @@
 
-function crearPersona(nombre, apellido) {
-    return {
-        nombre: nombre,
-        apellido: apellido
-    }
-}
+// function crearPersona(nombre, apellido) {
+//     return {
+//         nombre: nombre,
+//         apellido: apellido
+//     }
+// }
+
+const crearPersona = ( nombre, apellido ) => ({ nombre, apellido });
 
 const persona = crearPersona('Juan', 'Rodriguez');
+const { apellido: nuevoApellido } = crearPersona('Juan', 'Ramos');
 
 
 // simplificar
 
 function crearAnimal(animal, raza) {
     return {
-        animal, 
+        animal,
         raza
     }
 }
@@ -41,11 +44,47 @@ const imprimirArgumentos2 = ( edad, ...args ) => { //despues del parametro rest.
 }
 
 const argumentos = imprimirArgumentos2(10, 'Juan', true, false, 'Rodriguez');
+const [ edad, nombre, vivo, casado, apellido ] = imprimirArgumentos2(10, 'Juan', true, false, 'Jimenez');
 
+
+
+// destructuracion de argumentos
+
+const tony = {
+    nombre: 'Tony Stark',
+    codeName: 'Ironman',
+    vivo: false,
+    edad: 40, 
+    trajes: ['Mark I', 'Mark V', 'Hulkbuster'],
+};
+
+// const imprimePropiedades = ( personaje ) => {
+
+//     console.log( personaje.nombre );
+//     console.log( personaje.codeName );
+//     console.log( personaje.vivo );
+//     console.log( personaje.edad );
+//     console.log( personaje.trajes );
+
+// }
+
+const imprimePropiedades = ( { nombre, codeName, vivo, edad = 15, trajes} ) => {
+    
+    console.log({nombre});
+    console.log({codeName});
+    console.log({vivo});
+    console.log({edad});
+    console.log({trajes});
+
+}
+
+imprimePropiedades( tony );
 
 
 // console.log(persona);
 // console.log(animales);
 // console.log(objetos);
 // imprimirArgumentos(10, 'Juan', true, false, 'Rodriguez');
-console.log({ argumentos });
+// console.log({ argumentos });
+// console.log({ edad, nombre, vivo, casado, apellido });
+// console.log({ nuevoApellido });
